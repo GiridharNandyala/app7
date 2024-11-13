ecf7f63eb7a1b3e4d16c5fe9766262d4c6fb27fa
giridharnadyala <giridharnadyala757@gmail.com>
2024-11-13 14:14:12 +0530

    hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { ServicesComponent } from './services/services.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,ServicesComponent,ContactusComponent,AboutComponent,NavbarComponent],
+  imports: [RouterOutlet,HomeComponent,SignupComponent,ServicesComponent,LoginComponent,ContactusComponent,AboutComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { HomeComponent } from './home/home.component'; // Adjust the path as nee
 import { AboutComponent } from './about/about.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { ServicesComponent } from './services/services.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent } ,
   {path:'about',component:AboutComponent},
   {path:'contactus',component:ContactusComponent},
   {path:'services',component:ServicesComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+
+
     // This will set the home route to the HomeComponent
 ];
